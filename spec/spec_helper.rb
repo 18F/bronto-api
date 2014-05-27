0bc1ad4ee3c37d50ede44321f87542cb446d17c4
Alan deLevie <adelevie@gmail.com>
2014-05-27 16:42:41 -0400

    quick and dirty and hackish fix to get specs running

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,25 @@ SimpleCov.start
 require 'rspec_api_blueprint'
 
 require File.expand_path '../../app/bronto.rb', __FILE__
-require 'spec/support/json'
-require 'spec/support/sinatra'
+#require 'spec/support/json'
+#require 'spec/support/sinatra'
+
+module Bronto
+  module Test
+    module Helpers
+      def json
+        @json ||= JSON.parse(@api.last_response.body)
+      end
+    end
+  end
+end
+
+require 'rack/test'
+
+module SinatraApps
+	def app() Bronto::App end
+	def api() Bronto::API end
+end
  
 RSpec.configure do |c|
   c.include Rack::Test::Methods
